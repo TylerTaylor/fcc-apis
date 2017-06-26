e46c8d41a7369ae18b27b6734a8fcc4df73ef879
tylertaylor <vikingtyty@gmail.com>
2017-06-26 12:37:01 -0500

    setup the imageSearch api project in this main project

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var multer = require('multer');
-var upload = multer({dest: 'uploads/'})
+var upload = multer({dest: 'uploads/'});
+
+var mongoose = require('mongoose');
+var imageSearchDb = process.env.MONGOLAB_IMGDB_URI
+var imageConn = mongoose.createConnection(imageSearchDb);
+
+var Schema = mongoose.Schema;
+var https = require('https')
+
+var recentSearchSchema = new Schema({
+  term: String,
+  when: String
+})
+
+var RecentSearch = imageConn.model('RecentSearch', recentSearchSchema)
 
 /* GET home page. */
 router.get('/', (req, res) => {
@@ -74,5 +88,64 @@ router.get('/fileMetadata', (req, res) => {
 router.post('/fileMetadata/upload', upload.single('file'), (req, res) => {
   return res.json(req.file)
 })
+
+/* IMAGE SEARCH */
+var baseUrl = "https://www.googleapis.com/customsearch/v1?"
+var cx = process.env.CSE_CX
+var key = process.env.CSE_KEY
+var apiUrl = baseUrl + "cx=" + cx + "&key=" + key + "&q="
+
+const request = require('request')
+
+router.get('/imagesearch/latest', (req, res) => {
+  RecentSearch.find({}, {_id: 0, __v: 0}, (err, data) => {
+    res.json(data)
+  })
+})
+
+router.get('/imagesearch/:query*', (req, res) => {
+  var queryParam = req.params.query
+  var { offset } = req.query
+
+  var saveSearchTerm = new RecentSearch({
+    term: queryParam,
+    when: new Date()
+  })
+
+  saveSearchTerm.save()
+
+  if (offset) {
+    var query = apiUrl + queryParam + "&searchType=image&start=" + offset
+    sendRequest(query)
+  } else {
+    var query = apiUrl + queryParam + "&searchType=image"
+    sendRequest(query)
+  }
+
+  function sendRequest(query) {
+    request(query, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        let queryResults = JSON.parse(body)
+        let results = []
+        queryResults = queryResults['items']
+
+        for (var i=0; i < queryResults.length; i++) {
+          let obj = {
+            url: queryResults[i]['link'],
+            snippet: queryResults[i]['snippet'],
+            thumbnail: queryResults[i]['image']['thumbnailLink'],
+            context: queryResults[i]['image']['contextLink']
+          }
+          results.push(obj)
+        }
+
+        res.json(results)
+      } else {
+        console.log("Got an error: ", error, ", status code: ", response.statusCode)
+      }
+    })
+  }
+})
+
 
 module.exports = router;
